0f5efaff6f232649800c8360fdbc0d2bb6ee44bf
Ryan Smith <rms1000watt@gmail.com>
2018-07-16 10:21:43 -0700

    updated generator

--- generator.sh
+++ generator.sh
@@ -6,14 +6,13 @@ MAX_EVENTS=5
 echo "Requires:"
 echo "    sudo pip install jinja2 jinja2-cli"
 echo
-
 echo '{"max_functions":'"$MAX_FUNCTIONS"',"max_events":'"$MAX_EVENTS"'}' > j2.json
-# jinja2 templates/local-lambda.tf.j2 j2.json
 
 for file in templates/*.j2; do
   out=${file//.j2/}
   out=${out//templates\//}
-  echo "jinja2 $file j2.json > $out"
+  echo "Generating: $out"
+  # jinja2 "$file" j2.json > "$out"
 done
 
 rm j2.json
