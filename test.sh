a3e2ec3799a4387a58ed5ade0a4c26b7ec9ff2d6
Ryan Smith <rms1000watt@gmail.com>
2018-08-13 14:47:04 -0700

    updated cleanup to multi region

--- test.sh
+++ test.sh
@@ -21,9 +21,14 @@ for dir in examples/*; do
   sleep 5
 done
 
-for logGroup in $(aws logs describe-log-groups | jq '.logGroups[].logGroupName' | tr -d '"'); do
-  echo "Deleting log group: $logGroup"
-  aws logs delete-log-group --log-group-name "$logGroup"
+for region in us-east-1 us-east-2 us-west-1 us-west-2 ap-south-1 ap-northeast-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ca-central-1 sa-east-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3; do
+  export AWS_REGION=$region
+  export AWS_DEFAULT_REGION=$region
+  echo "Handling region: $region"
+  for logGroup in $(aws logs describe-log-groups | jq '.logGroups[].logGroupName' | tr -d '"'); do
+    echo "Deleting log group: $logGroup"
+    aws logs delete-log-group --log-group-name "$logGroup"
+  done
 done
 
 aws s3 rb s3://org-x-super-cool-bucket
